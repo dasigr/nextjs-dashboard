05a181f6cf5ab52ef58767c62b951ce4123f89d1
Romualdo Dasig <dasig.rg@a5project.com>
2025-03-01 11:14:20 +0800

    Remove artificial query delays

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,15 +13,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -32,20 +24,12 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log('Fetching latest invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-    
-    console.log('Data fetch completed after 4 seconds.');
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
@@ -60,12 +44,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log('Fetching card data...');
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -75,8 +53,6 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-    
-    console.log('Data fetch completed after 2 seconds.');
 
     const data = await Promise.all([
       invoiceCountPromise,
