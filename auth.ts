06155ebacf695e1e77cb23f8265e558f3675aa11
Romualdo Dasig <dasig.rg@a5project.com>
2025-03-02 18:12:56 +0800

    Authenticate through a Drupal API

--- auth.ts
+++ auth.ts
@@ -3,82 +3,66 @@ import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User, Token } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
-import postgres from 'postgres';
 import axios from 'axios';
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return user[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
 
 async function getToken(name: string, password: string) {
   try {
-    const url = `${process.env.DRUPAL_API_URL}/oauth/token`
+    const url = `${process.env.DRUPAL_API_URL}/oauth/token`;
 
     const data = new URLSearchParams()
-    data.append('grant_type', 'password')
-    data.append('client_id', `${process.env.DRUPAL_CLIENT_ID}`)
-    data.append('client_secret', `${process.env.DRUPAL_CLIENT_SECRET}`)
-    data.append('username', `${name}`)
-    data.append('password', `${password}`)
-    data.append('scope', 'finance')
+    data.append('grant_type', 'password');
+    data.append('client_id', `${process.env.DRUPAL_CLIENT_ID}`);
+    data.append('client_secret', `${process.env.DRUPAL_CLIENT_SECRET}`);
+    data.append('username', `${name}`);
+    data.append('password', `${password}`);
+    data.append('scope', 'finance');
 
     const response = await axios.post(url, data, {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded'
       }
-    })
+    });
 
-    return response.data
-
+    return response.data;
   } catch (error) {
-    console.error("Error fetching token:", error)
+    console.error("Error fetching token:", error);
   }
 }
 
 async function getUserId(token: Token) {
   try {
-    const url = `${process.env.DRUPAL_API_URL}/jsonapi`
+    const url = `${process.env.DRUPAL_API_URL}/jsonapi`;
 
     const response = await axios.get(url, {
       headers: {
         'Accept': 'application/vnd.api+json',
         'Authorization': 'Bearer ' + token.access_token
       }
-    })
+    });
 
-    return response.data.meta.links.me.meta.id
-    
+    return response.data.meta.links.me.meta.id;
   } catch (error) {
-    console.error("Error fetching user:", error)
+    console.error("Error fetching user:", error);
   }
 }
 
-async function getUserById(user_id: string, token: Token) {
+async function getUser(user_id: string, token: Token) {
   try {
-    const url = `${process.env.DRUPAL_API_URL}/jsonapi/user/user/${user_id}`
+    const url = `${process.env.DRUPAL_API_URL}/jsonapi/user/user/${user_id}`;
 
     const response = await axios.get(url, {
       headers: {
         'Accept': 'application/vnd.api+json',
         'Authorization': 'Bearer ' + token.access_token
       }
-    })
+    });
 
     // console.log('User', response.data)
-    return response.data
-    
+    return response.data;
   } catch (error) {
-    console.error("Error fetching user details:", error)
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
   }
 }
 
@@ -102,7 +86,7 @@ export const { auth, signIn, signOut } = NextAuth({
           // console.log('User ID', user_id);
 
           // Get User.
-          const user = await getUserById(user_id, token);
+          const user = await getUser(user_id, token);
           // console.log('User', user);
 
           if (!user) return null;
